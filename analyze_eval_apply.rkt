2e0ccfd0b25c761d4d53c9e78317c652aad33a44
pbruyninckx <pbruyninckx@gmail.com>
2019-03-30 13:15:00 +0000

    4.1.7 (section) Analyze expression before evaluation

--- analyze_eval_apply.rkt
+++ analyze_eval_apply.rkt
@@ -2,45 +2,32 @@
 
 (define apply-in-underlying-scheme apply)
 
-(define (eval exp env)
+(define (eval exp env) ((analyze exp) env))
+
+(define (analyze exp)
   (cond ((self-evaluating? exp)
-         exp)
+         (analyze-self-evaluating exp))
         ((variable? exp)
-         (lookup-variable-value exp env))
+         (analyze-variable exp))
         ((quoted? exp)
-         (text-of-quotation exp))
+         (analyze-quoted exp))
         ((assignment? exp)
-         (eval-assignment exp env))
+         (analyze-assignment exp))
         ((definition? exp)
-         (eval-definition exp env))
+         (analyze-definition exp))
         ((if? exp)
-         (eval-if exp env))
+         (analyze-if exp))
         ((lambda? exp)
-         (make-procedure
-          (lambda-parameters exp)
-          (lambda-body exp)
-          env))
+         (analyze-lambda exp))
         ((begin? exp)
-         (eval-sequence
-          (begin-actions exp)
-          env))
-        ((and? exp)
-         (eval-and exp env))
-        ((or? exp)
-         (eval (or->if exp) env))
+         (analyze-sequence
+          (begin-actions exp)))
         ((cond? exp)
-         (eval (cond->if exp) env))
-        ((let? exp)
-         (eval (let->combination exp) env))
-        ((let*? exp)
-         (eval (let*->nested-lets exp)))
+         (analyze (cond->if exp)))
         ((application? exp)
-         (metacircular-apply (eval (operator exp) env)
-                (list-of-values
-                 (operands exp)
-                 env)))
+         (analyze-application exp))
         (else
-         (error "Unknown expression type: EVAL " exp))))
+         (error "Unknown expression type: ANALYZE " exp))))
 
 (define (metacircular-apply procedure arguments)
   (cond ((primitive-procedure? procedure)
@@ -58,6 +45,84 @@
             procedure))))
         (else
          (error "Unknown procedure type: METACIRCULAR-APPLY " procedure))))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation exp)))
+    (lambda (env) qval)))
+
+(define (analyze-variable exp)
+  (lambda (env)
+    (lookup-variable-value exp env)))
+
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env)
+      (set-variable-value!
+       var (vproc env) env)
+      'ok)))
+
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+        (vproc (analyze
+                (definition-value exp))))
+    (lambda (env)
+      (define-variable! var (vproc env) env)
+      'ok)))
+
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp))))
+    (lambda (env)
+      (if (true? (pproc env))
+          (cproc env)
+          (aproc env)))))
+
+(define (analyze-lambda ecp)
+  (let ((vars (lambda-parameters exp))
+        (bproc (analyze-sequence
+                (lambda-body exp))))
+    (lambda (env)
+      (make-procedure vars bproc env))))
+
+(define (analyze-sequence exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env) (proc1 env) (proc2 env)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc
+                            (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence: ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env)
+      (execute-application
+       (fproc env)
+       (map (lambda (aproc) (aproc env))
+            aprocs)))))
+
+(define (execute-application proc args)
+  (cond ((primitive-procedure? proc)
+         (apply-primitive-procedure proc args))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment
+           (procedure-parameters proc)
+           args
+           (procedure-environment proc))))
+        (else (error "Unknown procedure type: EXECUTE-APPLICATION"
+                     proc))))
 
 (define (list-of-values exps env)
   (if (no-operands? exps)
@@ -490,12 +555,12 @@
 (define the-global-environment
   (setup-environment))
 
-(eval '(define (map op vals)
-         (if (null? vals)
-             vals
-             (cons (op (car vals))
-                   (map op (cdr vals)))))
-      the-global-environment)
+;(eval '(define (map op vals)
+;         (if (null? vals)
+;             vals
+;             (cons (op (car vals))
+;                   (map op (cdr vals)))))
+;      the-global-environment)
 
 ;(driver-loop)
 
